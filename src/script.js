0f6f0388f8d9b7cf5807b4a08e30c68704453876
Gabriel Amaral <gabrielamaral@Gabriels-MacBook-Air.local>
2022-09-22 10:19:40 -0600

    adding lists function made

--- src/script.js
+++ src/script.js
@@ -1,6 +1,10 @@
+let name = 0
 function makingLists() {
     let input = document.querySelector('#listNames');
     let text = input.value
     let lists = document.querySelector('.lists');
-    lists.innerHTML += `<div>${text}</div>`
+    lists.innerHTML += `<input type="radio" name="a0" id="${name}" class="radio" style="display:none">
+    <label for="${name}">${input.value}</label>`
+    name += 1
 }
+
